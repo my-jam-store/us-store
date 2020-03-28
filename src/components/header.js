7edaf297d34a14b09afd1311888bae115268129b
shawkiS <shawki@oceanprotocol.com>
2020-03-28 20:29:28 +0300

    Change footer

--- src/components/header.js
+++ src/components/header.js
@@ -85,10 +85,14 @@ function Header() {
         </div>
       </div>
 
-      <div className="md:mt-0 md:order-1">
-        <p className="text-right text-base leading-6 text-gray-400">
-          &copy; All rights reserved for MyJam @ 2020
+      <div class="bg-white">
+        <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+          <div class="mt-8 md:mt-0 md:order-1">
+            <p class="text-right text-base leading-6 text-gray-400">
+              &copy; All rights reserved for MyJam @ 2020
       </p>
+          </div>
+        </div>
       </div>
     </div>
   );
